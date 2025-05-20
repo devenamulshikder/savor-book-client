32e80a698e75f31242bfb4f121866c9ae53b5834
devenamulshikder <devenamulshikder@gmail.com>
2025-05-20 13:47:00 +0600

    feat: integrate Firebase authentication
    - Added Firebase dependency to package.json.
    - Created firebase configuration file to initialize Firebase app.
    - Implemented AuthProvider component for user authentication, including methods for creating users, logging in, Google sign-in, updating user profiles, and sending password reset emails.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAfWv-r6WKulHQN7PZO-pmGn2auxqAnftY",
+  authDomain: "savor-book.firebaseapp.com",
+  projectId: "savor-book",
+  storageBucket: "savor-book.firebasestorage.app",
+  messagingSenderId: "1055618952567",
+  appId: "1:1055618952567:web:cda741d974f2d062370572",
+};
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,78 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from "react";
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  //   create user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   signin user
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // sign in with google
+  const googleProvider = new GoogleAuthProvider();
+  const googleLogIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // update user
+  const updateUser = (updatedData) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, updatedData);
+  };
+  // send password reset email
+  const forgotPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authData = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    error,
+    setError,
+    createUser,
+    loginUser,
+    googleLogIn,
+    updateUser,
+    forgotPassword,
+  };
+
+  return (
+    <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
