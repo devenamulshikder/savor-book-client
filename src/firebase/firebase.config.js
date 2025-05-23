77125cc40ed055689262a8dd5d93d1f1322d38ef
devenamulshikder <devenamulshikder@gmail.com>
2025-05-23 16:56:35 +0600

    fix: update Firebase configuration environment variable names for consistency

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: import.meta.env.VITE_FIREBASE_api,
+  authDomain: import.meta.env.VITE_FIREBASE_auth,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId,
 };
 const app = initializeApp(firebaseConfig);
 export default app;
