d6457d7e8a6ade0826e579cf9f0fd969105dcc42
devenamulshikder <devenamulshikder@gmail.com>
2025-05-23 16:58:49 +0600

    fix: correct authDomain environment variable name in Firebase configuration

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_api,
-  authDomain: import.meta.env.VITE_FIREBASE_auth,
+  authDomain: import.meta.env.VITE_FIREBASE,
   projectId: import.meta.env.VITE_FIREBASE_projectId,
   storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
   messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
